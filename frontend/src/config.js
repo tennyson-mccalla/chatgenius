637252c7e6510806395be99e133efeeb50d55369
Tennyson McCalla <tennyson.mccalla@gmail.com>
2025-01-08 15:57:47 -0500

    Remove unused Auth components and update config files

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://localhost:3001';
+export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001';
+export const WS_URL = process.env.REACT_APP_WS_URL || 'ws://localhost:3001';
